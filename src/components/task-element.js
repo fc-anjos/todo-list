043f63e3ad9665c5bab8e4918449efa2dea526d7
fc-anjos <felipe.anjos@usp.br>
2020-12-09 20:27:38 -0300

    Refactor todo-element to hold the event listeners

--- src/components/task-element.js
+++ src/components/task-element.js
@@ -1,22 +1,69 @@
 import styles from '../styles/task_element.module.css';
 
 import { projectOption, editTaskForm } from './task-form';
-import { hideEl, showEl } from '../utils/show-hide';
 
 import mergeNodes from '../utils/merge-nodes';
 
-const handleEditTaskForm = (e, projects) => {
-  e.preventDefault();
-  const formData = new FormData(e.target);
-  const taskInfo = Object.fromEntries(formData);
-  const project = projects[taskInfo.projectIndex];
-  const updatedProject = project.replaceTask(taskInfo);
-  const updatedProjects = projects;
-  updatedProjects[taskInfo.projectIndex] = updatedProject;
-  hideEl('task-form-container');
-  showEl('add-new-task');
-  return updatedProjects;
+const todoElement = (task, taskIndex, projectIndex, projects) => {
+  const fragment = new DocumentFragment();
+  const div = document.createElement('div');
+  div.innerHTML = todoTag(task, taskIndex, projectIndex);
+  const editBtn = div.querySelector('.edit-btn');
+  editBtn.addEventListener('click', () => {
+    updateTask(task, taskIndex, projectIndex, projects);
+  });
+  fragment.appendChild(div);
+  return fragment;
 };
+
+const taskElements = (content, projectIndex, projects) => {
+  const fragment = new DocumentFragment();
+  const todoElements = content.map(
+    (task, taskIndex) => todoElement(task, taskIndex, projectIndex, projects),
+  );
+  const todoElementsContainer = mergeNodes(todoElements);
+  fragment.appendChild(todoElementsContainer);
+  return fragment;
+};
+
+const populateForm = (form, task, projects) => {
+  const titleField = form.querySelector('#title');
+  titleField.value = task.title;
+
+  const descriptionField = form.querySelector('#description');
+  descriptionField.value = task.description;
+
+  const priorityField = form.querySelector('#priority');
+  priorityField.value = task.priority;
+
+  const dateStringField = form.querySelector('#dateString');
+  dateStringField.value = task.formattedDate();
+
+  const projectField = form.querySelector('#project-select');
+  projectField.innerHTML = projects.map(projectOption).join('');
+};
+
+const projectElement = (project, projectIndex, projects) => {
+  const fragment = new DocumentFragment();
+  const title = project.name;
+  const content = project.tasks;
+  const isEmpty = content.length === 0;
+
+  if (isEmpty) {
+    fragment.innerHTML = `
+     <div>
+       <i>
+         Project ${title} has no todos
+       </i>
+     </div>
+     `;
+  } else {
+    fragment.appendChild(taskElements(content, projectIndex, projects));
+  }
+  return fragment;
+};
+
+
 const todoTag = (task, taskIndex, projectIndex) => {
   const {
     title,
@@ -54,21 +101,15 @@ const todoTag = (task, taskIndex, projectIndex) => {
 `;
 };
 
-const populateForm = (form, task, projects) => {
-  const titleField = form.querySelector('#title');
-  titleField.value = task.title;
-
-  const descriptionField = form.querySelector('#description');
-  descriptionField.value = task.description;
-
-  const priorityField = form.querySelector('#priority');
-  priorityField.value = task.priority;
-
-  const dateStringField = form.querySelector('#dateString');
-  dateStringField.value = task.formattedDate();
-
-  const projectField = form.querySelector('#project-select');
-  projectField.innerHTML = projects.map(projectOption).join('');
+const handleEditTaskForm = (e, projects) => {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  const taskInfo = Object.fromEntries(formData);
+  const project = projects[taskInfo.projectIndex];
+  const updatedProject = project.replaceTask(taskInfo);
+  const updatedProjects = projects;
+  updatedProjects[taskInfo.projectIndex] = updatedProject;
+  return updatedProjects;
 };
 
 const updateTask = (task, taskIndex, projectIndex, projects) => {
@@ -79,51 +120,9 @@ const updateTask = (task, taskIndex, projectIndex, projects) => {
   populateForm(form, task, projects);
   form.addEventListener('submit', e => {
     const updatedProjects = handleEditTaskForm(e, projects);
+    // TODO: CHANGE FOR REPLACE SPECIFIC TASK
     drawTodos(updatedProjects);
-    addEditDeleteEventListener(updatedProjects);
   });
-};
-
-const todoElement = (task, taskIndex, projectIndex, projects) => {
-  const fragment = new DocumentFragment();
-  const div = document.createElement('div');
-  div.innerHTML = todoTag(task, taskIndex, projectIndex);
-  const editBtn = div.querySelector('.edit-btn');
-  editBtn.addEventListener('click', () => {
-    updateTask(task, taskIndex, projectIndex, projects);
-  });
-  fragment.appendChild(div);
-  return fragment;
-};
-
-const taskElements = (title, content, projectIndex, projects) => {
-  const fragment = new DocumentFragment();
-  // fragment.innerHTML = title;
-  const todoElements = content.map((task, taskIndex) => todoElement(task, taskIndex, projectIndex, projects));
-  const todoElementsContainer = mergeNodes(todoElements);
-  fragment.appendChild(todoElementsContainer);
-  return fragment;
-};
-
-// Takes a project and converts it in an array of tasks tags
-const projectElement = (project, projectIndex, projects) => {
-  const fragment = new DocumentFragment();
-  const title = project.name;
-  const content = project.tasks;
-  const isEmpty = content.length === 0;
-
-  if (isEmpty) {
-    fragment.innerHTML = `
-     <div>
-       <i>
-         Project ${title} has no todos
-       </i>
-     </div>
-     `;
-  } else {
-    fragment.appendChild(taskElements(title, content, projectIndex, projects));
-  }
-  return fragment;
 };
 
 
@@ -132,7 +131,6 @@ const drawTodos = projects => {
   const projectElements = projects.map(projectElement);
   container.innerHTML = '';
   container.appendChild(mergeNodes(projectElements));
-  // updateProjectOptions(projects);
 };
 
 
